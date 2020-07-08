9084bace43141d75cf5cd9fbeffa7850d3cdfd79
Arturo Cano <arturo.cano@globant.com>
2020-07-08 15:53:52 -0500

    ADD has methods to TypeAWS enum

--- java/src/main/java/org/globant/enums/TypesAws.java
+++ java/src/main/java/org/globant/enums/TypesAws.java
@@ -1,5 +1,8 @@
 package org.globant.enums;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum TypesAws {
     DMS_SUBSCRIPTION("AWS::DMS::EventSubscription", "EventSubscription"),
     DMS_SUBNET_GROUP("AWS::DMS::ReplicationSubnetGroup", "SubnetGroup"),
@@ -14,19 +17,24 @@ public enum TypesAws {
     STACK("AWS::CloudFormation::Stack", "Stack"),
     TOPIC("AWS::SNS::Topic", "Topic");
 
-    private final String key;
-    private final String value;
+    private static Set<String> KEYS = new HashSet<>();
+    private static Set<String> VALUES = new HashSet<>();
 
-    TypesAws(String key, String value) {
-        this.key = key;
-        this.value = value;
+    static {
+        for(TypesAws typeAws: TypesAws.values()) {
+            KEYS.add(typeAws.getKey());
+        }
+        for(TypesAws typeAws: TypesAws.values()) {
+            VALUES.add(typeAws.getValue());
+        }
     }
 
-    public String getKey() {
-        return key;
+    public static boolean hasKey(String key) {
+        return KEYS.contains(key);
     }
-    public String getValue() {
-        return value;
+
+    public static boolean hasValue(String value) {
+        return VALUES.contains(value);
     }
 
     public static TypesAws fromKey(String key) {
@@ -45,5 +53,20 @@ public enum TypesAws {
             }
         }
         return null;
+    }
+
+    private final String key;
+    private final String value;
+
+    TypesAws(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+    public String getValue() {
+        return value;
     }
 }
